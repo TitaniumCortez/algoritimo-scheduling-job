bbc0ef2a5d954b33fbd0852eade8285740aa286d
RenanCortez <renancortez141@gmail.com>
2020-05-21 00:16:28 -0300

    trocado agenda objeto Map para Array

--- src/schedulingJobs.js
+++ src/schedulingJobs.js
@@ -1,44 +1,23 @@
 const moment = require('moment');
+const data = require('./data');
+const Props = require('./config');
 
-const janela_de_execucao = {
-    dataInicio: '2019-11-10 09:00:00',
-    dataFim: '2019-11-11 12:00:00',
-    periodoExecucao: 8
-};
 
-const data = [
-    {
-        "id": 1,
-        "description": "Importação de arquivos de fundos",
-        "maxDate": "2019-11-10 12:00:00",
-        "estimatedTime": "2 horas,"
-    },
-    {
-        "id": 2,
-        "description": "Importação de dados da Base Legada",
-        "maxDate": " 2019-11-11 12:00:00",
-        "estimatedTime": "4 horas",
-    },
-    {
-        "id": 3,
-        "description": "Importação de dados de integração",
-        "maxDate": "2019-11-11 08:00:00",
-        "estimatedTime": "6 horas",
-    }
-]
-const hora_inicio_janela = moment(janela_de_execucao.dataInicio).hours();
-const hora_fim_janela = moment(janela_de_execucao.dataFim).hours();
-const diferenca_de_dias = dayDiff(janela_de_execucao);
+const hora_inicio_janela = moment(Props.dataInicio).hours();
+const hora_fim_janela = moment(Props.dataFim).hours();
+const diferenca_de_dias = dayDiff(Props);
 const dias = new Array(diferenca_de_dias);
-const agenda = new Map();
+const agenda = [];
+
 
 for (let i = 0; i < dias.length; i++) {
     //Inicializa os dias
     dias[i] = new Array(24);
     //Inicializa agenda
-    agenda.set(i, { hours: 0, data: new Array() });
+    agenda.push({ hours: 0, data: new Array() });
 }
 
+//adiciona demilitador no periodo da janela
 dias[0][hora_inicio_janela] = '-';
 dias[dias.length - 1][hora_fim_janela] = '-';
 
@@ -60,39 +39,55 @@ dias.forEach((dia, index) => {
     }
 })
 
-//ordenacao de prioridade de agendamento por date e horario
+//ordena para os primeiros horarios a serem agendados
+data.sort((a, b) => moment(a.maxDate) - moment(b.maxDate))
 
 
 console.table(dias);
+console.log(data);
+
+//Verefica se a data informada esta dentro do periodo da janela
+const dentroPeriodoJanela = (element) => {
+    return moment(element.maxDate).isBetween(Props.dataInicio, Props.dataFim)
+        || moment(element.maxDate).isSame(Props.dataFim)
+        || moment(element.maxDate).isSame(Props.dataInicio);
+}
+
+// Retorna a diferenca da data em dias
+function dayDiff({ dataInicio, dataFim }) {
+    return moment(dataFim).diff(dataInicio, 'days') + 1;
+}
 
 // Agendamento de job
 for (let i = 0; i < data.length; i++) {
     const element = data[i];
     const hora_estimada_job = Number(element.estimatedTime.charAt(0));
-    const maximo_time_execucao
+    //const maximo_time_execucao
     let agendado = false;
 
-
-    if (agendado)
+    //Finaliza o loop quando ocorre o agendamento
+    if (agendado || !dentroPeriodoJanela(element))
         break;
 
     for (let index = 0; index < dias.length; index++) {
-        let horas_agendada = agenda.get(index).hours;
+        //recupera da agenda os horarios agendados
+        let horas_agendada = agenda[index].hours;
         let dia = dias[index];
 
+        //Finaliza o loop quando ocorre o agendamento
         if (agendado)
             break;
 
         for (let hora = 0; hora < dia.length; hora++) {
             //Analisa disponibilidade de agenda
-            if (dia[hora] == 0 && (horas_agendada + hora_estimada_job) <= janela_de_execucao.periodoExecucao
-                &&             ) {
+            if (dia[hora] == 0 && (horas_agendada + hora_estimada_job) <= Props.periodoExecucao) {
                 horas_agendada += hora_estimada_job;
 
                 //Agenda o job com valor 1
                 dia.fill(1, hora, hora + hora_estimada_job);
 
-                const result = agenda.get(index);
+                //adiciona na agenda as horas 
+                const result = agenda[index];
                 result.hours = horas_agendada;
                 result.data.push(element.id);
 
@@ -104,24 +99,6 @@ for (let i = 0; i < data.length; i++) {
     }
 }
 
-console.log(agenda.get(0));
-console.log(agenda.get(1));
-
-
-console.table(dias);
-
-function dentroPeriodoJanela(element) {
-    return moment(element.maxDate).isBetween(janela_de_execucao.dataInicio, janela_de_execucao.dataFim)
-        || moment(element.maxDate).isSame(janela_de_execucao.dataFim)
-        || moment(element.maxDate).isSame(janela_de_execucao.dataInicio);
-}
-
-function dayDiff({ dataInicio, dataFim }) {
-    return moment(dataFim).diff(dataInicio, 'days') + 1;
-}
-
-const sortDate = () => {
-    
-}
-
-// [1, 3] , [2]
+const result = agenda.map((item) => { return item.data });
+console.log(result);
+// [ [1, 3] , [2] ] 
